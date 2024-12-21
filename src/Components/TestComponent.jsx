bcb03775714666a2d311de208676c6f6ff30bfc7
UncannyPotato69 <get.it.together.beetch@gmail.com>
2024-12-21 16:13:13 +0530

    Something's better than nothing

--- src/Components/TestComponent.jsx
+++ src/Components/TestComponent.jsx
@@ -2,11 +2,28 @@ import React from "react";
 
 import classes from "./TestComponent.module.css";
 
+import returnSquares from "./squares";
+import { Squares2X2Icon } from "@heroicons/react/16/solid";
+
 const TestComponent = () => {
+  const sqaures = returnSquares();
+
   return (
-    <div className={`${classes.test}`}>
-      <p>💙💜💛🧡🤍🩷🖤💚</p>
-    </div>
+    <svg width={400} height={400}>
+      {sqaures.map((e, i) => {
+        return (
+          <rect
+            height={400 / 64}
+            width={400 / 64}
+            x={i * (400 / 64)}
+            y={(64 - (i + 1)) * (400 / 64)}
+            key={i + `${e.squareName}`}
+            fill={e.white ? "white" : "limegreen"}
+            name={e.squareName}
+          ></rect>
+        );
+      })}
+    </svg>
   );
 };
 
